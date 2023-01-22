db32adb91f71932cc871b88856ca87e84a602cb9
cvonjoua <cvonjoua@asu.edu>
2023-01-22 12:16:44 -0800

    master adjust improper request warning

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -369,7 +369,8 @@ class WebServer {
                     builder.append("HTTP/1.1 422 Bad Request\n ");
                     builder.append("Content-Type: text/html; charset=utf-8\n");
                     builder.append("\n");
-                    builder.append("{\"Error\": \"Non number value detected.  Parameter: " + variableName + " is not a valid number.  Please correct input.\"}");
+                    builder.append("Number Format Exception - Line 372 - Not enough inputs.  Please specify 2 inputs - Example:  /gradecheck?num1<...>&num2<...>");
+                    //builder.append("{\"Error\": \"Non number value detected.  Parameter: " + variableName + " is not a valid number.  Please correct input.\"}");
                 }
             } catch (NumberFormatException e) {
                 builder.append("HTTP/1.1 422 Bad Request\n ");
