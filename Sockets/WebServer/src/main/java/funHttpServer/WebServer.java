2d4f505e54d7ac0171f6ad80ed3abaccbe78f444
cvonjoua <cvonjoua@asu.edu>
2023-01-22 23:05:18 -0800

    master undoing additions in github, error testing

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -244,46 +244,37 @@ class WebServer {
           //     then drill down to what you care about
           // "Owner's repo is named RepoName. Example: find RepoName's contributors" translates to
           //     "/repos/OWNERNAME/REPONAME/contributors"
-            
-            Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           
-            try {
-              query_pairs = splitQuery(request.replace("github?", ""));
-              String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
-              System.out.println(json);
+          Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+          query_pairs = splitQuery(request.replace("github?", ""));
+          String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
+          System.out.println(json);
 
+          builder.append("HTTP/1.1 200 OK\n");
+          builder.append("Content-Type: text/html; charset=utf-8\n");
+          builder.append("\n");
+          builder.append("Check the todos mentioned in the Java source file <br/>");
+          // TODO: Parse the JSON returned by your fetch and create an appropriate
+          // response based on what the assignment document asks for
           
-              //builder.append("HTTP/1.1 200 OK\n");
-              //builder.append("Content-Type: text/html; charset=utf-8\n");
-              //builder.append("\n");
-              //builder.append("Check the todos mentioned in the Java source file <br/>");
-              // TODO: Parse the JSON returned by your fetch and create an appropriate
-              // response based on what the assignment document asks for
+          JSONArray repoArray = new JSONArray(json);
+          JSONArray newArray = new JSONArray();
+          
+          for (int i = 0; i < repoArray.length(); i++) {
+              JSONObject repo = repoArray.getJSONObject(i);
               
-              JSONArray repoArray = new JSONArray(json);
-              JSONArray newArray = new JSONArray();
+              String repoName = repo.getString("name");
+              Integer repoId = repo.getInt("id");
               
-              for (int i = 0; i < repoArray.length(); i++) {
-                  JSONObject repo = repoArray.getJSONObject(i);
-                  
-                  String repoName = repo.getString("name");
-                  Integer repoId = repo.getInt("id");
-                  
-                  JSONObject ownerRepo = repo.getJSONObject("owner");
-                  String authorName = ownerRepo.getString("login");
-                  
-                  builder.append("HTTP/1.1 200 OK\n");
-                  builder.append("Repo:  " + repoName + "<br/>");
-                  builder.append("ID:  " + repoId + "<br/>");
-                  builder.append("Owner:  " + authorName + "<br/>");
-                  builder.append("<hr/>");
-              }
-            } catch (Exception e) {
-                builder.append("HTTP/1.1 422 Bad Request\n");
-                builder.append("Content-Type: text/html; charset=utf-8\n");
-                builder.append("\n");
-                builder.append("Your query is incomplete, please review and try again.  Example:  host:PORT/github?query=users/[USERNAME]/repos\n");
-            }
+              JSONObject ownerRepo = repo.getJSONObject("owner");
+              String authorName = ownerRepo.getString("login");
+              
+              builder.append("HTTP/1.1 200 OK\n");
+              builder.append("Repo:  " + repoName + "<br/>");
+              builder.append("ID:  " + repoId + "<br/>");
+              builder.append("Owner:  " + authorName + "<br/>");
+              builder.append("<hr/>");
+          }
           
         } else if (request.contains("charcount")) {
             
