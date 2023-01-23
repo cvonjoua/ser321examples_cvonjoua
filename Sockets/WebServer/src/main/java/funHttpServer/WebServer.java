771470616ef0bae005a962c947ad5769defdd65d
cvonjoua <cvonjoua@asu.edu>
2023-01-22 23:09:21 -0800

    master commented out append statements

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -251,10 +251,10 @@ class WebServer {
               String json = fetchURL("https://api.github.com/" + query_pairs.get("query"));
               System.out.println(json);
     
-              builder.append("HTTP/1.1 200 OK\n");
-              builder.append("Content-Type: text/html; charset=utf-8\n");
-              builder.append("\n");
-              builder.append("Check the todos mentioned in the Java source file <br/>");
+              //builder.append("HTTP/1.1 200 OK\n");
+              //builder.append("Content-Type: text/html; charset=utf-8\n");
+              //builder.append("\n");
+              //builder.append("Check the todos mentioned in the Java source file <br/>");
               // TODO: Parse the JSON returned by your fetch and create an appropriate
               // response based on what the assignment document asks for
               
