3b849255102d992aee33914bd904d64cde5ba176
cvonjoua <cvonjoua@asu.edu>
2023-01-22 23:12:59 -0800

    master test2 append statements for errors

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -253,7 +253,7 @@ class WebServer {
     
               builder.append("HTTP/1.1 200 OK\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
-              builder.append("\n");
+              //builder.append("\n");
               //builder.append("Check the todos mentioned in the Java source file <br/>");
               // TODO: Parse the JSON returned by your fetch and create an appropriate
               // response based on what the assignment document asks for
